f69fdd3773dc7b87bdb989ddb2656e64c234dd82
Dark869 <ing.victor97@gmail.com>
2024-12-29 17:24:43 -0600

    Se agrego el modelado de la base de datos

--- db/models.py
+++ db/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    full_name = models.CharField(max_length=255)
+    nick = models.CharField(max_length=100, unique=True)
+    email = models.EmailField(unique=True)
+    passwd = models.CharField(max_length=128)
+
+class Keys(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, to_field='id')
+    private_key_file = models.TextField()
+    public_key_file = models.BinaryField()
+    iv = models.TextField()
